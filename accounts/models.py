c01167fc452aeec38b115568a9c1de3abba8a984
Guilherme Saito <hguilherme433@gmail.com>
2024-07-09 15:38:30 -0400

    Some Changes & Fixing

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# first_name(string)
+# last_name(string)
+# username(string)
+# email(email)
+# password(password)
---
+++ accounts/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render (request, "index.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match.")
+            return redirect('signup')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exist.")
+            return redirect('signup')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exist.")
+            return redirect('signup')
+
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your Account has ben successfully created!")
+
+        return redirect('signin')
+
+    return render (request, "register.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "home_authenticated.html", {'fname' : fname})
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+
+
+    return render (request, "login.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out Successfully!")
+    return redirect('home')
